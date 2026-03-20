ec24baddad26476813798522973a3e0893aa54fa
ritikshakya0049 <ritikshakya0049@gmail.com>
2026-03-20 13:20:07 +0000

    learning about objects in js

---
+++ 01_variable/object.js
@@ -0,0 +1,27 @@
+const student={
+    fullName: "ritik kumar",
+    age:20,
+    isPass:true,
+    marks:[90,80,70],
+    address:{
+        city:"gautam budh nagar",
+        country:"india"
+    }
+}
+student.fullName="Ritik shakya";
+student.age=student.age+1;
+student.address.city="greater noida";
+console.log(student.fullName);
+console.log(student.age);
+console.log(student.isPass);
+console.log(student.marks);
+console.log(student.address);
+const product={
+    title:"ball pen",
+    price:70,
+    rating:4.5,
+    isAvailable:true    
+
+} 
+console.log(product);
+console.log(student);
